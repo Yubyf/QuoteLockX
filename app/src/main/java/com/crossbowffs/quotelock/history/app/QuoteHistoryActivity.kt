47db42d0a4aebbd5472bac05759b77fae0fd2270
Yubyf <lpy19920505@gmail.com>
2022-03-24 23:03:59 +0800

    Added a clear menu item in the quote histories page.

--- app/src/main/java/com/crossbowffs/quotelock/history/app/QuoteHistoryActivity.kt
+++ app/src/main/java/com/crossbowffs/quotelock/history/app/QuoteHistoryActivity.kt
@@ -1,13 +1,23 @@
 package com.crossbowffs.quotelock.history.app
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.crossbowffs.quotelock.R
+import com.crossbowffs.quotelock.history.provider.QuoteHistoryContract
+import com.crossbowffs.quotelock.utils.ioScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * @author Yubyf
  */
 class QuoteHistoryActivity : AppCompatActivity() {
+    private lateinit var mMenu: Menu
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_container)
@@ -15,5 +25,30 @@ class QuoteHistoryActivity : AppCompatActivity() {
             .beginTransaction()
             .replace(R.id.content_frame, QuoteHistoryFragment())
             .commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.histories_options, menu)
+        mMenu = menu
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        clearQuoteHistories()
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun clearQuoteHistories() {
+        mMenu.findItem(R.id.clear)?.isEnabled = false
+        ioScope.launch {
+            contentResolver.delete(QuoteHistoryContract.Histories.CONTENT_URI, null, null)
+            withContext(Dispatchers.Main) {
+                mMenu.findItem(R.id.clear)?.isEnabled = true
+                Toast.makeText(this@QuoteHistoryActivity,
+                    R.string.quote_histories_cleared_quote,
+                    Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
     }
 }
