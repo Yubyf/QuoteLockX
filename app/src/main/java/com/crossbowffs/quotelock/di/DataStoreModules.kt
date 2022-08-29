Yubyf/QuoteLockX

commit 6e4b4e7021fb6b36bb7b7e0c9b3ed15ada0a2b5d

Removed the implementation of PreferenceDataStore for deprecated PreferenceScreen.

app/src/main/java/com/crossbowffs/quotelock/di/DataStoreModules.kt
@@ -5,8 +5,6 @@ import com.crossbowffs.quotelock.app.configs.brainyquote.BrainyQuotePrefKeys
 import com.crossbowffs.quotelock.app.configs.fortune.FortunePrefKeys
 import com.crossbowffs.quotelock.app.configs.hitokoto.HitokotoPrefKeys
 import com.crossbowffs.quotelock.consts.PREF_COMMON
-import com.crossbowffs.quotelock.consts.PREF_QUOTES
-import com.crossbowffs.quotelock.data.datastore.PreferenceDataStoreAdapter
 import com.crossbowffs.quotelock.data.modules.jinrishici.JinrishiciQuoteModule
 import com.yubyf.datastore.DataStoreDelegate
 import com.yubyf.datastore.DataStoreDelegate.Companion.getDataStoreDelegate
@@ -24,15 +22,7 @@ annotation class CommonDataStore
 
 @Qualifier
 @Retention(AnnotationRetention.RUNTIME)
-annotation class CommonDataStoreAdapter
-
-@Qualifier
-@Retention(AnnotationRetention.RUNTIME)
 annotation class QuotesDataStore
-
-@Qualifier
-@Retention(AnnotationRetention.RUNTIME)
-annotation class QuotesDataStoreAdapter
 
 @Qualifier
 @Retention(AnnotationRetention.RUNTIME)
@@ -61,22 +51,10 @@ object DataStoreModules {
         context.getDataStoreDelegate(PREF_COMMON, migrate = true)
 
     @Singleton
-    @CommonDataStoreAdapter
-    @Provides
-    fun provideCommonDataStoreAdapter(@ApplicationContext context: Context): PreferenceDataStoreAdapter =
-        PreferenceDataStoreAdapter(context, PREF_COMMON, true)
-
-    @Singleton
     @QuotesDataStore
     @Provides
     fun provideQuotesDataStore(@ApplicationContext context: Context): DataStoreDelegate =
         context.getDataStoreDelegate(PREF_COMMON, migrate = true)
-
-    @Singleton
-    @QuotesDataStoreAdapter
-    @Provides
-    fun provideQuotesDataStoreAdapter(@ApplicationContext context: Context): PreferenceDataStoreAdapter =
-        PreferenceDataStoreAdapter(context, PREF_QUOTES, true)
 
     @Singleton
     @BrainyDataStore

app/build.gradle.kts
@@ -180,7 +180,7 @@ dependencies {
     // Jetpack Compose
     val navVersion = "2.5.1"
     implementation("androidx.activity:activity-compose:1.5.1")
-    implementation("androidx.compose.material3:material3:1.0.0-alpha16")
+    implementation("androidx.compose.material3:material3:1.0.0-beta01")
     implementation("androidx.compose.animation:animation:1.2.1")
     implementation("androidx.compose.ui:ui-tooling:1.2.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.2.1")

app/src/main/java/com/crossbowffs/quotelock/data/datastore/PreferenceDataStoreAdapter.kt
@@ -1,62 +1,0 @@
-package com.crossbowffs.quotelock.data.datastore
-
-import android.content.Context
-import androidx.datastore.preferences.core.Preferences
-import androidx.preference.PreferenceDataStore
-import com.yubyf.datastore.DataStoreDelegate.Companion.getDataStoreDelegate
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.runBlocking
-
-/**
- * @author Yubyf
- */
-class PreferenceDataStoreAdapter(context: Context, name: String, migrate: Boolean = false) :
-    PreferenceDataStore() {
-
-    private val dataStoreDelegate = context.getDataStoreDelegate(name, migrate = migrate)
-
-    override fun putString(key: String, value: String?) = dataStoreDelegate.put(key, value)
-
-    override fun putStringSet(key: String, values: MutableSet<String>?) =
-        dataStoreDelegate.put(key, values)
-
-    override fun putInt(key: String, value: Int) = dataStoreDelegate.put(key, value)
-
-    override fun putLong(key: String, value: Long) = dataStoreDelegate.put(key, value)
-
-    override fun putFloat(key: String, value: Float) = dataStoreDelegate.put(key, value)
-
-    override fun putBoolean(key: String, value: Boolean) = dataStoreDelegate.put(key, value)
-
-    override fun getString(key: String, defValue: String?): String? =
-        runBlocking { dataStoreDelegate.getStringSuspend(key, defValue) }
-
-    override fun getStringSet(key: String, defValues: Set<String>?): Set<String> =
-        runBlocking { dataStoreDelegate.getStringSetSuspend(key, defValues) ?: emptySet() }
-
-    override fun getInt(key: String, defValue: Int): Int =
-        runBlocking { dataStoreDelegate.getIntSuspend(key, defValue) }
-
-    override fun getLong(key: String, defValue: Long): Long =
-        runBlocking { dataStoreDelegate.getLongSuspend(key, defValue) }
-
-    override fun getFloat(key: String, defValue: Float): Float =
-        runBlocking { dataStoreDelegate.getFloatSuspend(key, defValue) }
-
-    override fun getBoolean(key: String, defValue: Boolean): Boolean =
-        runBlocking { dataStoreDelegate.getBooleanSuspend(key, defValue) }
-
-    fun bulkPut(map: Map<String, *>) {
-        dataStoreDelegate.bulkPut(map)
-    }
-
-    fun remove(key: String) = dataStoreDelegate.remove(key)
-
-    fun collect(collector: suspend (Preferences, Preferences.Key<*>?) -> Unit): Job =
-        dataStoreDelegate.collect(collector)
-
-    suspend fun collectSuspend(collector: suspend (Preferences, Preferences.Key<*>?) -> Unit) =
-        dataStoreDelegate.collectSuspend(collector)
-
-    fun contains(key: String): Boolean = runBlocking { dataStoreDelegate.containsSuspend(key) }
-}

app/src/main/java/com/crossbowffs/quotelock/data/modules/QuoteLocalSource.kt
@@ -3,21 +3,22 @@ package com.crossbowffs.quotelock.data.modules
 import androidx.datastore.preferences.core.Preferences
 import com.crossbowffs.quotelock.consts.*
 import com.crossbowffs.quotelock.data.api.QuoteData
-import com.crossbowffs.quotelock.data.datastore.PreferenceDataStoreAdapter
 import com.crossbowffs.quotelock.data.history.QuoteHistoryEntity
 import com.crossbowffs.quotelock.data.history.QuoteHistoryRepository
 import com.crossbowffs.quotelock.data.modules.collections.QuoteCollectionRepository
-import com.crossbowffs.quotelock.di.QuotesDataStoreAdapter
+import com.crossbowffs.quotelock.di.QuotesDataStore
 import com.crossbowffs.quotelock.di.ResourceProvider
 import com.crossbowffs.quotelock.utils.Xlog
 import com.crossbowffs.quotelock.utils.md5
+import com.yubyf.datastore.DataStoreDelegate
 import com.yubyf.quotelockx.R
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class QuoteLocalSource @Inject constructor(
-    @QuotesDataStoreAdapter private val quotesDataStoreAdapter: PreferenceDataStoreAdapter,
+    @QuotesDataStore private val quotesDataStore: DataStoreDelegate,
     private val collectionRepository: QuoteCollectionRepository,
     private val historyRepository: QuoteHistoryRepository,
     private val resourceProvider: ResourceProvider,
@@ -49,7 +50,7 @@ class QuoteLocalSource @Inject constructor(
             insertQuoteHistory(quoteText, quoteSource, quoteAuthor)
         }
         val collectionState = queryQuoteCollectionState(quoteText, quoteSource, quoteAuthor)
-        quotesDataStoreAdapter.bulkPut(
+        quotesDataStore.bulkPut(
             mapOf(PREF_QUOTES_TEXT to quoteText,
                 PREF_QUOTES_SOURCE to quoteSource,
                 PREF_QUOTES_AUTHOR to quoteAuthor,
@@ -58,28 +59,31 @@ class QuoteLocalSource @Inject constructor(
     }
 
     fun setQuoteCollectionState(state: Boolean) {
-        quotesDataStoreAdapter.putBoolean(PREF_QUOTES_COLLECTION_STATE, state)
+        quotesDataStore.put(PREF_QUOTES_COLLECTION_STATE, state)
     }
 
-    fun getLastUpdateTime() = quotesDataStoreAdapter.getLong(PREF_QUOTES_LAST_UPDATED, -1)
+    fun getLastUpdateTime() =
+        runBlocking { quotesDataStore.getLongSuspend(PREF_QUOTES_LAST_UPDATED, -1) }
 
-    fun getCurrentQuote() = QuoteData(
-        quotesDataStoreAdapter.getString(PREF_QUOTES_TEXT, "")!!,
-        quotesDataStoreAdapter.getString(PREF_QUOTES_SOURCE, "")!!,
-        quotesDataStoreAdapter.getString(PREF_QUOTES_AUTHOR, "")!!,
-    )
+    fun getCurrentQuote() = runBlocking {
+        QuoteData(
+            quotesDataStore.getStringSuspend(PREF_QUOTES_TEXT, "")!!,
+            quotesDataStore.getStringSuspend(PREF_QUOTES_SOURCE, "")!!,
+            quotesDataStore.getStringSuspend(PREF_QUOTES_AUTHOR, "")!!,
+        )
+    }
 
-    fun notifyBooted() {
-        val hasBootNotifyFlag = quotesDataStoreAdapter.contains(PREF_BOOT_NOTIFY_FLAG)
+    fun notifyBooted() = runBlocking {
+        val hasBootNotifyFlag = quotesDataStore.containsSuspend(PREF_BOOT_NOTIFY_FLAG)
         if (!hasBootNotifyFlag) {
-            quotesDataStoreAdapter.putInt(PREF_BOOT_NOTIFY_FLAG, 0)
+            quotesDataStore.put(PREF_BOOT_NOTIFY_FLAG, 0)
         } else {
-            quotesDataStoreAdapter.remove(PREF_BOOT_NOTIFY_FLAG)
+            quotesDataStore.remove(PREF_BOOT_NOTIFY_FLAG)
         }
     }
 
     suspend fun observeQuoteDataStore(collector: suspend (Preferences, Preferences.Key<*>?) -> Unit) =
-        quotesDataStoreAdapter.collectSuspend(collector)
+        quotesDataStore.collectSuspend(collector)
 
     companion object {
         private const val TAG = "QuoteDownloader"

app/src/main/java/com/crossbowffs/quotelock/data/modules/QuoteRemoteSource.kt
@@ -5,10 +5,10 @@ import com.crossbowffs.quotelock.consts.PREF_COMMON_QUOTE_MODULE
 import com.crossbowffs.quotelock.consts.PREF_COMMON_QUOTE_MODULE_DEFAULT
 import com.crossbowffs.quotelock.data.api.QuoteData
 import com.crossbowffs.quotelock.data.api.QuoteModule
-import com.crossbowffs.quotelock.data.datastore.PreferenceDataStoreAdapter
-import com.crossbowffs.quotelock.di.CommonDataStoreAdapter
+import com.crossbowffs.quotelock.di.CommonDataStore
 import com.crossbowffs.quotelock.di.IoDispatcher
 import com.crossbowffs.quotelock.utils.Xlog
+import com.yubyf.datastore.DataStoreDelegate
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineDispatcher
@@ -19,7 +19,7 @@ import javax.inject.Singleton
 @Singleton
 class QuoteRemoteSource @Inject constructor(
     @ApplicationContext private val context: Context,
-    @CommonDataStoreAdapter private val commonDataStoreAdapter: PreferenceDataStoreAdapter,
+    @CommonDataStore private val commonDataStore: DataStoreDelegate,
     @IoDispatcher private val dispatcher: CoroutineDispatcher,
 ) {
 
@@ -31,7 +31,7 @@ class QuoteRemoteSource @Inject constructor(
     }
 
     private suspend fun Context.fetchQuote(): QuoteData? {
-        val moduleName: String = commonDataStoreAdapter.getString(PREF_COMMON_QUOTE_MODULE,
+        val moduleName: String = commonDataStore.getStringSuspend(PREF_COMMON_QUOTE_MODULE,
             PREF_COMMON_QUOTE_MODULE_DEFAULT)!!
 
         Xlog.d(TAG, "Attempting to download new quote...")

commit bcda83dec580993c6ef3ab9245285219a8ecf57c

Fixed an issue with quotes not updating on the lockscreen caused by commit 6e4b4e70.

app/src/main/java/com/crossbowffs/quotelock/di/DataStoreModules.kt
@@ -5,6 +5,7 @@ import com.crossbowffs.quotelock.app.configs.brainyquote.BrainyQuotePrefKeys
 import com.crossbowffs.quotelock.app.configs.fortune.FortunePrefKeys
 import com.crossbowffs.quotelock.app.configs.hitokoto.HitokotoPrefKeys
 import com.crossbowffs.quotelock.consts.PREF_COMMON
+import com.crossbowffs.quotelock.consts.PREF_QUOTES
 import com.crossbowffs.quotelock.data.modules.jinrishici.JinrishiciQuoteModule
 import com.yubyf.datastore.DataStoreDelegate
 import com.yubyf.datastore.DataStoreDelegate.Companion.getDataStoreDelegate
@@ -54,7 +55,7 @@ object DataStoreModules {
     @QuotesDataStore
     @Provides
     fun provideQuotesDataStore(@ApplicationContext context: Context): DataStoreDelegate =
-        context.getDataStoreDelegate(PREF_COMMON, migrate = true)
+        context.getDataStoreDelegate(PREF_QUOTES, migrate = true)
 
     @Singleton
     @BrainyDataStore