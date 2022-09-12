77932c9c05ecfb58504e79c94986b18852c61eb1
Yubyf <lpy19920505@gmail.com>
2022-09-12 13:26:54 +0800

    Removed unused codes.

--- app/src/main/java/com/crossbowffs/quotelock/data/api/QuoteViewData.kt
+++ app/src/main/java/com/crossbowffs/quotelock/data/api/QuoteViewData.kt
@@ -1,34 +1,3 @@
 package com.crossbowffs.quotelock.data.api
 
-import android.content.res.Resources
-import com.crossbowffs.quotelock.consts.PREF_QUOTE_SOURCE_PREFIX
-import com.yubyf.quotelockx.R
-
 data class QuoteViewData(val text: String = "", val source: String = "")
-
-fun Resources.buildQuoteViewData(
-    text: String,
-    source: String?,
-    author: String?,
-): QuoteViewData {
-    return QuoteViewData(
-        text,
-        author.let {
-            if (!it.isNullOrBlank()) {
-                "$PREF_QUOTE_SOURCE_PREFIX$it${if (source.isNullOrBlank()) "" else " $source"}"
-            } else {
-                if (source == getString(R.string.module_custom_setup_line2)
-                    || source == getString(R.string.module_collections_setup_line2)
-                ) {
-                    source
-                } else {
-                    if (source.isNullOrBlank()) {
-                        ""
-                    } else {
-                        "$PREF_QUOTE_SOURCE_PREFIX$source"
-                    }
-                }
-            }
-        }
-    )
-}
