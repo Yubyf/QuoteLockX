3e55b08ab8c0bdbcc3188ddb3a5709658050ba40
Yubyf <lpy19920505@gmail.com>
2022-09-20 19:32:55 +0800

    Removed unused blocks.

--- app/src/main/java/com/crossbowffs/quotelock/app/main/MainActivity.kt
+++ app/src/main/java/com/crossbowffs/quotelock/app/main/MainActivity.kt
@@ -36,52 +36,6 @@ class MainActivity : ComponentActivity() {
                     onDispose {}
                 }
                 MainNavHost()
-//                var snapshot: Bitmap? by remember {
-//                    mutableStateOf(null)
-//                }
-//                val snapshotStates = mutableListOf<Snapshot>()
-//                var size: IntSize? by remember {
-//                    mutableStateOf(null)
-//                }
-//                Column {
-//                    Column(modifier = Modifier
-//                        .onSizeChanged {
-//                            size = it
-//                        }
-//                        .clickable {
-//                            size?.let {
-//                                snapshot = Bitmap
-//                                    .createBitmap(it.width, it.height, Bitmap.Config.ARGB_8888)
-//                                    .also { bitmap ->
-//                                        val canvas = Canvas(bitmap)
-//                                        canvas.drawColor(Color.Yellow.toArgb())
-//                                        snapshotStates.forEach { snapshot ->
-//                                            snapshot.snapshot(canvas)
-//                                        }
-//                                    }
-//                            }
-//                        }) {
-//                        SnapshotText(modifier = Modifier
-//                            .padding(16.dp)
-//                            .background(Color.Red),
-//                            text = "落霞与孤鹜齐飞，秋水共长天一色",
-//                            snapshotState = rememberSnapshotState().also {
-//                                snapshotStates += it
-//                            }
-//                        )
-//                        SnapshotText(modifier = Modifier
-//                            .padding(16.dp)
-//                            .background(Color.Red),
-//                            text = "——王勃",
-//                            snapshotState = rememberSnapshotState().also {
-//                                snapshotStates += it
-//                            }
-//                        )
-//                    }
-//                    snapshot?.let {
-//                        Image(bitmap = it.asImageBitmap(), contentDescription = null)
-//                    }
-//                }
             }
         }
     }
